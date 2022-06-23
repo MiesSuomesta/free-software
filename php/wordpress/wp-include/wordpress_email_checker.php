4e0ebc55772d4e1690e1d2376daac03bc3b44505
Lauri Jakku <lja@lja.fi>
2022-06-23 23:21:00 +0300

    Hmmm...OK?

--- php/wordpress/wp-include/wordpress_email_checker.php
+++ php/wordpress/wp-include/wordpress_email_checker.php
@@ -4,7 +4,7 @@ include_once("checkEmail.php");
 
 function lja_mailer_raw_check_email($targetaddress, $verbose = 0)
 {
-	return lja_email_checker_raw_check_email($targetaddress, $verbose); 
+	return \PHPMailer\PHPMailer\lja_email_checker_raw_check_email($targetaddress, $verbose); 
 }
 
 function lja_mailer_check_email($targetaddress, $ver)
@@ -21,6 +21,7 @@ function lja_mailer_check_email($targetaddress, $ver)
 
 function lja_is_email_valid($email)
 {
+	error_log("LJA email check of " . $email, 0);
         $rv = lja_mailer_check_email($email, true);
 	error_log("LJA email check: " . $email .": $rv", 0);
         return $rv;
